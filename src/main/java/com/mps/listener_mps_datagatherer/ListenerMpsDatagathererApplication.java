8b522552449a98023d62f5120b97f563c6529569
Mohit Prakash <33515107+mohit-prakash@users.noreply.github.com>
2023-09-22 19:27:27 +0530

    first commit

---
+++ src/main/java/com/mps/listener_mps_datagatherer/ListenerMpsDatagathererApplication.java
@@ -0,0 +1,13 @@
+package com.mps.listener_mps_datagatherer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerMpsDatagathererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenerMpsDatagathererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mps/listener_mps_datagatherer/ListenerMpsDatagathererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mps.listener_mps_datagatherer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenerMpsDatagathererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
